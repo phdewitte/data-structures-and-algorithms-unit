425ecdd6a0c66e6a6a6fa2e2848ae2f49f5a79ae
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-11-10 18:52:23 -0600

    Add tests and methods for insert_last and remove_last

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -18,5 +18,24 @@ class LinkedList
     @head.insert_after(previous_head)
   end
 
+  def remove_first
+    previous_next = @head.next
+    @head = previous_next
+  end
 
+  def insert_last(element)
+    node_to_analyze = @head
+    until node_to_analyze.next == nil
+      node_to_analyze = node_to_analyze.next
+    end
+    node_to_analyze.insert_after(Node.new(element))
+  end
+
+  def remove_last
+    node_to_analyze = @head
+    until node_to_analyze.next.next == nil
+      node_to_analyze = node_to_analyze.next
+    end
+    node_to_analyze.remove_after
+  end
 end
